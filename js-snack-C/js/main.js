0f06fc3e086a99c1ea9245644d59d283625010f1
Ezy93 <giordani001@gmail.com>
2022-03-11 15:12:14 +0100

    aggiustata identatura codice

--- js-snack-C/js/main.js
+++ js-snack-C/js/main.js
@@ -1,18 +1,18 @@
 /**
  * 
-    A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l'iniziale maiuscola.
+   A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l'iniziale maiuscola.
 
-    Es: ['pippo', 'PLUTO', 'Paperino'] => ['Pippo', 'Pluto', 'Paperino']
+   Es: ['pippo', 'PLUTO', 'Paperino'] => ['Pippo', 'Pluto', 'Paperino']
 
  * 
  */
 
-    const stringArray = ["giovanni","emanuele", "giuseppe", "antonio", "legolas","GESUALDO", "aLFONSO", "Gertrude"];
+   const stringArray = ["giovanni","emanuele", "giuseppe", "antonio", "legolas","GESUALDO", "aLFONSO", "Gertrude"];
 
-    let newFormat = stringArray.map((elemento) => { 
-        
-        return elemento.charAt(0).toUpperCase() +
-        elemento.substring(1).toLowerCase();
-    
+   let newFormat = stringArray.map((elemento) => { 
+      
+      return elemento.charAt(0).toUpperCase() +
+      elemento.substring(1).toLowerCase();
+
 } )
-    console.table(newFormat)
+   console.table(newFormat)
