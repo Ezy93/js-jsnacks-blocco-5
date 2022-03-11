e3e542b669cc2099c9cf450a4879252d3be86f1e
Ezy93 <giordani001@gmail.com>
2022-03-11 12:44:43 +0100

    js-snack-C

---
+++ js-snack-C/js/main.js
@@ -0,0 +1,18 @@
+/**
+ * 
+    A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l'iniziale maiuscola.
+
+    Es: ['pippo', 'PLUTO', 'Paperino'] => ['Pippo', 'Pluto', 'Paperino']
+
+ * 
+ */
+
+    const stringArray = ["giovanni","emanuele", "giuseppe", "antonio", "legolas","GESUALDO", "aLFONSO", "Gertrude"];
+
+    let newFormat = stringArray.map((elemento) => { 
+        
+        return elemento.charAt(0).toUpperCase() +
+        elemento.substring(1).toLowerCase();
+    
+} )
+    console.table(newFormat)
